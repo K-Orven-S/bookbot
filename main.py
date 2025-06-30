714dd7810bd296f51991cf39fef0a385c7d8ecdb
kimorvens <kimorven@gmail.com>
2025-06-30 07:04:35 -0400

    finalize everything

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from stats import get_book_words
+from stats import count_char
+from stats import sort_list
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -6,8 +13,21 @@ def get_book_text(file_path):
         return file_contents
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    text = get_book_text(sys.argv[1])
     book_words = get_book_words(text)
-    print(f"{book_words} words found in the document")
-
+    word_count = count_char(text)
+    sorted_list = sort_list(word_count)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {book_words} total words")
+    print("--------- Character Count -------")
+    for item in sorted_list:
+        for key in item:
+            if key.isalpha():
+                print(f"{key}: {item[key]}")
+            else:
+                pass
+    print("============= END ===============")
+ 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,28 @@
 def get_book_words(book_text):
     number_of_words = len(book_text.split())
     return number_of_words
+
+def count_char(book_text):
+    lower_case = book_text.lower()
+    letters = {}
+    for letter in lower_case:
+        if letter not in letters:
+            letters[letter] = 1
+        else:
+            letters[letter] += 1
+    return letters
+
+def sort_on(items):
+    for key in items:
+        value = items[key]
+    return value
+
+def sort_list(char_dict):
+    char_list_of_dic = []
+    for key in char_dict:
+        one_char_dic = {}
+        one_char_dic[key] = char_dict[key]
+        char_list_of_dic.append(one_char_dic)
+    sorted_list = char_list_of_dic.sort(reverse=True, key=sort_on)
+    return char_list_of_dic
+
