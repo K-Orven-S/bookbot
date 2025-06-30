24ebface041dfeeb9210019ff146bc11b757f076
kimorvens <kimorven@gmail.com>
2025-06-30 02:20:16 -0400

    added get_book_text and get_book_words function

---
+++ main.py
@@ -0,0 +1,13 @@
+from stats import get_book_words
+
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+        return file_contents
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    book_words = get_book_words(text)
+    print(f"{book_words} words found in the document")
+
+main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_book_words(book_text):
+    number_of_words = len(book_text.split())
+    return number_of_words
